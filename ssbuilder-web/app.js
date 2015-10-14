a6596b56c7140de5b57e5490c67b047621fc7bde
Sonia Sen <ssen10@email.arizona.edu>
2015-10-14 18:18:55 -0400

    Added 'view' page with Full Slider bootstrap capability that is accessed from clicking on the story

--- ssbuilder-web/app.js
+++ ssbuilder-web/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var stories = require('./routes/stories');
 var students = require('./routes/students');
+var view = require('./routes/view');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/', stories);
 app.use('/', students);
+app.use('/', view);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
